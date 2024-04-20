f86cb8eb787c1b8c96a9866b742a2dce6c47f2c0
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2024-04-21 01:56:50 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 64 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -10,25 +10,36 @@ using namespace std;
 
 const int MAX = 20;
 int n, cnt = 0;
-vector<vector<int>> graph(MAX, vector<int>(MAX, 1));
+vector<vector<int>> graph;
 
 void input(){
     cin >> n;
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
+    graph.resize(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             cin >> graph[i][j];
         }
     }
 }
 
-void backtracking(int r, int c, int dir){
-    if(graph[r][c]) return;
-    if(r == n && c == n) { cnt++; return; }
-    if(dir == 1) backtracking(r, c + 1, 1);
-    if(dir == 2) backtracking(r + 1, c, 2);
-    if(dir == 3) backtracking(r, c + 1, 1), backtracking(r + 1, c, 2);
-    if(!graph[r][c+1] && !graph[r+1][c] && !graph[r+1][c+1])
-        backtracking(r + 1, c + 1, 3);
+void dfs(int x, int y, int dir){
+    if (x == n - 1 && y == n - 1){
+        cnt++;
+        return;
+    }
+    if (dir == 0 || dir == 2){
+        if (y + 1 < n && graph[x][y + 1] == 0){
+            dfs(x, y + 1, 0);
+        }
+    }
+    if (dir == 1 || dir == 2){
+        if (x + 1 < n && graph[x + 1][y] == 0){
+            dfs(x + 1, y, 1);
+        }
+    }
+    if (x + 1 < n && y + 1 < n && graph[x + 1][y] == 0 && graph[x][y + 1] == 0 && graph[x + 1][y + 1] == 0){
+        dfs(x + 1, y + 1, 2);
+    }
 }
 
 int main() {
@@ -36,6 +47,6 @@ int main() {
     cin.tie(0);
     cout.tie(0);
     input();
-    backtracking(1, 2, 1);
+    dfs(0, 1, 0);
     cout << cnt << "\n";
 }
