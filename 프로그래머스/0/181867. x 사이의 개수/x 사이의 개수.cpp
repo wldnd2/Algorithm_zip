d27136bc6cba7beeb518cd9574f24119428c8eb2
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2024-04-24 15:01:46 +0900

    [level 0] Title: x 사이의 개수, Time: 3.52 ms, Memory: 6.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string myString) {
+    vector<int> answer;
+    int cnt = 0;
+    for (int i = 0; i < myString.size(); i++){
+        if (myString[i] == 'x'){
+            answer.push_back(cnt);
+            cnt = 0;
+        }
+        else cnt++;   
+    }
+    answer.push_back(cnt);
+    return answer;
+}
