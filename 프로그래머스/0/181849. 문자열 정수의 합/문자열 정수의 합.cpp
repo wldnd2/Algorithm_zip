07c196fd7ce024bfbbb3b6c53e0905a7ee55c02c
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2024-04-24 15:24:57 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(string num_str) {
+    int answer = 0;
+    for(char c: num_str){
+        answer += (c - '0');
+    }
+    return answer;
+}
