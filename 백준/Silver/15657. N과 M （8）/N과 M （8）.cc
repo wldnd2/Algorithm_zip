a12720b2967c6f53d8f746c202cba7e82c16cda8
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2025-09-15 21:29:13 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <string>
+#include <queue>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> result;
+
+void init(){
+    cin >> n >> m;
+    nums.assign(n, 0);
+    for(int i = 0; i< n; i++){
+        cin >> nums[i];
+    }
+    sort(nums.begin(), nums.end());
+}
+
+void solve(int root_idx, int depth){
+    if (depth == m) {
+        for (int item: result) cout << item << " ";
+        cout << "\n";
+        return;
+    }
+    for (int i = root_idx; i < n; i++){
+        result.push_back(nums[i]);
+        solve(i, depth+1);
+        result.pop_back();
+    }
+}
+
+int main(void){
+    init();
+    solve(0, 0);
+}
