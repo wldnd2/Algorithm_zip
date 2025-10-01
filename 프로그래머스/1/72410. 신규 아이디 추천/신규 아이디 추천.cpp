ac2d1d676c9803d79bdff302a667cb7dc2839b79
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2025-10-01 17:09:16 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -6,14 +6,13 @@
 using namespace std;
 
 string solution(string new_id) {
-    string answer = "";
     // 1단계
     transform(new_id.begin(), new_id.end(), new_id.begin(), ::tolower);
 
     // 2단계
     // regex pattern("[a-z]\\d\\-\\_\\."); -> 이런 순차적으로 나오는다는 의미
     // 아래와 같이 사용해야지 해당 문자열내에 이러한 문자를 제외하는 패턴으로 적용한다는 의미
-    regex pattern("[^a-z\\d\\-_\\.]|");
+    regex pattern("[^a-z\\d\\-_\\.]");
     new_id = regex_replace(new_id, pattern, "");
 
     // 3단계
@@ -33,21 +32,17 @@ string solution(string new_id) {
     new_id = regex_replace(new_id, pattern4, "");
 
     // 7단계
-    int i = 0;
-    for (char c: new_id){
-        if (i == 15) break;
-        answer += c;
-        i++;
+    if (new_id.length() >= 16) {
+        new_id = new_id.substr(0, 15);
+        regex pattern_5("\\.$");
+        new_id = regex_replace(new_id, pattern_5, "");
     }
-    // 7단계
-    regex pattern5("\\.$");
-    answer = regex_replace(answer, pattern5, "");
 
     // 8단계
-    if (answer.size() <= 2){
-        while(answer.size() != 3){
-            answer += answer[answer.size()-1];
+    if (new_id.length() <= 2) {
+        while (new_id.length() < 3) {
+            new_id += new_id.back(); // back()으로 마지막 문자에 쉽게 접근
         }
     }
-    return answer;
+    return new_id;
 }
